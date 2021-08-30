2fb07cc08c60657fe045d788b1a0b762fb8a55a1
loljoho <loljoho@users.noreply.github.com>
2021-08-30 12:38:07 -0400

    Replace CRA with Bulma Login template

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-success is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div className="column is-4 is-offset-4">
+              <h3 className="title has-text-black">Login</h3>
+              <hr className="login-hr" />
+              <p className="subtitle has-text-black">Please login to proceed.</p>
+              <div className="box">
+                <figure className="avatar">
+                  <img alt="Logo" src={logo} />
+                </figure>
+                <form>
+                  <div className="field">
+                    <div className="control">
+                      <input className="input is-large" type="email" placeholder="Your Email" autofocus="" />
+                    </div>
+                  </div>
+
+                  <div className="field">
+                    <div className="control">
+                      <input className="input is-large" type="password" placeholder="Your Password" />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="checkbox">
+                      <input type="checkbox" /> Remember me
+                    </label>
+                  </div>
+                  <button className="button is-block is-info is-large is-fullwidth">Login <i className="fa fa-sign-in" aria-hidden="true"></i></button>
+                </form>
+              </div>
+              <p className="has-text-grey">
+                <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                <a href="../">Need Help?</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
